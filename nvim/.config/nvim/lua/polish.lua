49ec47811cf788f26efd5a2bc65cdfb2c5f9e233
dokxid <dan-du@live.de>
2026-04-10 22:27:30 +0200

    feat: add mpd, mpv and rmpc configs

--- nvim/.config/nvim/lua/polish.lua
+++ nvim/.config/nvim/lua/polish.lua
@@ -2,3 +2,11 @@
 -- This is just pure lua so anything that doesn't
 -- fit in the normal config locations above can go here
 -- require("lspconfig").glsl_analyzer.setup {}
+
+-- based on https://github.com/stevearc/aerial.nvim/issues/408#issuecomment-2336852084 after breaking, still need to figure out what triggered this
+local has_aer, aer = pcall(require, "aerial")
+if not has_aer then return end
+
+aer.setup {
+  backends = { "lsp", "markdown", "asciidoc", "man" },
+}
