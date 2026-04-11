97354eb0084fca69187a27f8242634745da95275
dokxid <dan-du@live.de>
2026-04-11 18:22:47 +0200

    fix: also restart termfilechooser portal before restarting xdg portals

--- hypr/.config/hypr/scripts/restart-portals.sh
+++ hypr/.config/hypr/scripts/restart-portals.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 sleep 4
 killall -e xdg-desktop-portal-hyprland
+killall -e xdg-desktop-portal-termfilechooser
 killall xdg-desktop-portal
 /usr/lib/xdg-desktop-portal-hyprland &
+/usr/lib/xdg-desktop-portal-termfilechooser &
 sleep 4
 /usr/lib/xdg-desktop-portal &
