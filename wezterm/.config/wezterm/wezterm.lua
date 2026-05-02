a0c96482941bde2939475ed3936543a3ffb4537b
dokxid <dan-du@live.de>
2026-05-02 16:35:22 +0200

    fix: wezterm copy paste based on context

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -18,7 +18,15 @@ local config = {
 		{
 			key = "c",
 			mods = "CTRL",
-			action = act.CopyTo("Clipboard"),
+			action = wezterm.action_callback(function(window, pane)
+				local selection_text = window:get_selection_text_for_pane(pane)
+				local is_selection_active = string.len(selection_text) ~= 0
+				if is_selection_active then
+					window:perform_action(wezterm.action.CopyTo("ClipboardAndPrimarySelection"), pane)
+				else
+					window:perform_action(wezterm.action.SendKey({ key = "c", mods = "CTRL" }), pane)
+				end
+			end),
 		},
 		{
 			key = "C",
