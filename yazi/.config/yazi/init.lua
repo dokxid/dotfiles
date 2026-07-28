ade90082a06e40f587d2c3564b69db78e9ef7871
dokxid <dan-du@live.de>
2026-07-28 21:59:04 +0200

    feat(yazi): clean up plugins and add augment-command.yazi

--- yazi/.config/yazi/init.lua
+++ yazi/.config/yazi/init.lua
@@ -10,6 +10,34 @@ else
 	require("starship"):setup()
 end
 
+-- https://github.com/hankertrix/augment-command.yazi#configuration
+require("augment-command"):setup({
+	prompt = false,
+	default_item_group_for_prompt = "hovered",
+	smart_enter = true,
+	smart_paste = false,
+	smart_tab_switch = true,
+	confirm_on_quit = true,
+	open_file_after_creation = false,
+	enter_directory_after_creation = false,
+	use_default_create_behaviour = false,
+	enter_archives = true,
+	extract_retries = 3,
+	recursively_extract_archives = true,
+	preserve_file_permissions = false,
+	encrypt_archives = false,
+	encrypt_archive_headers = false,
+	reveal_created_archive = true,
+	remove_archived_files = false,
+	must_have_hovered_item = true,
+	skip_single_subdirectory_on_enter = true,
+	skip_single_subdirectory_on_leave = true,
+	smooth_scrolling = false,
+	scroll_delay = 0.02,
+	wraparound_file_navigation = true,
+	protected_directories = {},
+})
+
 require("whoosh"):setup({
 	bookmarks = {
 		{ tag = "desktop", path = "~/Desktop", key = { "d", "D" } },
@@ -27,7 +55,7 @@ require("whoosh"):setup({
 	jump_notify = true,
 	home_alias_enabled = true,
 	special_keys = {
-		fuzzy_search = "<Space>",
+		fuzzy_search = "e",
 		history = "<Backspace>",
 		project_root = "-",
 	},
